cb504a7f3b9fe5158184092f790f8c090d91c8dc
Eugene <zekapogribnyak@gmail.com>
2023-04-01 12:35:41 +0300

    new_style_fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ module.exports = {
   theme: {
     fontFamily: {
       custom: ['Alkatra', 'sans-serif'],
+      'sans': ['"PT Sans"', 'sans-serif'],
+      'serif': ['"EB Garamond"', 'serif'],
+      'comfortaa': ['"Comfortaa"', 'cursive'],
+      'alkatra': ['"Alkatra"', 'sans-serif'],
+      'alkatra': ['Alkatra', 'sans-serif'],
+      'PT Sans': ['"PT Sans"', 'sans-serif'],
+      'EB Garamond': ['"EB Garamond"', 'serif'],
+      'Comfortaa': ['"Comfortaa"', 'cursive'],
+      'Merriweather': ['"Merriweather"', 'serif'],
+      'PT Sans Narrow': ['"PT Sans Narrow"', 'sans-serif'],
     },
     extend: {
       

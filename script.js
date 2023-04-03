aec6283f8aae3c33ad728920f9882872763de4ae
Eugene <zekapogribnyak@gmail.com>
2023-04-03 11:27:36 +0300

    script

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-const navbarToggle = document.querySelector('[data-collapse-toggle]');
-const navbarMenu = document.querySelector('#navbar-default');
+const menuButton = document.querySelector('.flex button[aria-controls="mobile-menu"]');
+const mobileMenu = document.getElementById('mobile-menu');
+const hamburgerIcon = document.querySelector('.hamburger-icon');
+  const closeIcon = document.querySelector('.close-icon');
 
-navbarToggle.addEventListener('click', () => {
-  navbarMenu.classList.toggle('hidden');
-  navbarMenu.classList.toggle('block');
+menuButton.addEventListener('click', () => {
+  const isExpanded = menuButton.getAttribute('aria-expanded') === 'true';
+  menuButton.setAttribute('aria-expanded', String(!isExpanded));
+  mobileMenu.classList.toggle('open');
+  mobileMenu.classList.toggle('hidden');
+  hamburgerIcon.classList.toggle('hidden');
+    closeIcon.classList.toggle('hidden');
+
+  // Отримуємо елемент <html>
+  const html = document.querySelector('html');
+
+  // Забороняємо/дозволяємо прокручування сторінки
+  if (mobileMenu.classList.contains('open')) {
+    html.style.overflow = 'hidden';
+  } else {
+    html.style.overflow = '';
+  }
 });
